24a2bc28a7745c99b14513992355b9d8f0580f3a
andrilblaze40 <andrilizu35@gmail.com>
2025-08-14 14:25:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -191,10 +191,10 @@ with st.expander('Accuracy Scores'):
 
 # Generate a classification report for your model's performance on the test data and assign it to `class_report`.
   class_report = classification_report(y_test, model.predict(X_test))
-  cr = pd.DataFrame(class_report)
+  class_report
 with st.expander('Classification Report'):
   st.write('**Class_Report**')
-  cr
+  class_report
   
 
 
