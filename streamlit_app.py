1f3596c48308a101a576b393db601703009209e8
andrilblaze40 <andrilizu35@gmail.com>
2025-08-14 02:11:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,6 +115,6 @@ with st.expander('Split Data'):
   over_sampler = RandomOverSampler(random_state=42)
   X_train_over, y_train_over = over_sampler.fit_resample(X_train, y_train)
   print("X_train_over shape:", X_train_over.shape)
-  X_train_over.head()                                       
+  X_train_over                                    
   
   
