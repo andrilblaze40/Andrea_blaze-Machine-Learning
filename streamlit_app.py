7c2ddd401acd8056f6cf4664c271ebfcea663fe1
andrilblaze40 <andrilizu35@gmail.com>
2025-08-14 03:02:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,5 +171,5 @@ with st.expander('Fit Model'):
   cv_results = pd.DataFrame(model.cv_results_)
 with st.expander('Cross Validation Results'):
   st.write('**cv_results**')
-  cv_results.head(5)
+  cv_results
 
