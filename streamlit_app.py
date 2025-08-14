436d1b078587f80ace0e5c3eae1910931edd07ed
andrilblaze40 <andrilizu35@gmail.com>
2025-08-14 02:16:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,10 +111,14 @@ with st.expander('Split Data'):
 
  
  
-
+#  Create a new feature matrix `X_train_over` and target vector `y_train_over` by performing random over-sampling on the training data.
   over_sampler = RandomOverSampler(random_state=42)
   X_train_over, y_train_over = over_sampler.fit_resample(X_train, y_train)
   print("X_train_over shape:", X_train_over.shape)
-  X_train_over                                    
+ 
+with st.expander(' RandomOverSampler'):
+  st.write('**over_sampler**')
+  X_train_over, y_train_over
+  
   
   
