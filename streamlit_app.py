b79f6cb3442b8ba492c4761fdcaf5d3a1aa42b23
andrilblaze40 <andrilizu35@gmail.com>
2025-08-14 10:20:43 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -191,11 +191,8 @@ with st.expander('Accuracy Scores'):
 
 # Plot a confusion matrix that shows how your model performed on your test set.
 # Generate confusion matrix
-  cm = confusion_matrix(model, X_test, y_test)
-  st.title("Confusion Matrix Display of Model Performance on Test set")
-  fig, ax = plt.subplots()
-  disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-  disp.plot(ax=ax, cmap="Blues")
+  ConfusionMatrixDisplay.from_estimator(model, X_test, y_test);
+  st.title("Confusion Matrix Display of Model Performance on Test set") 
   st.pyplot(fig)
 
 
